4ec0f8e8516483c8635fde0baaf7e68fba85ae33
kei ito <ito.kei@jvckenwood.com>
2023-09-06 16:43:27 +0900

    テストコード追加中、コードスニペット追加

---
+++ Domain_Test/Person_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Domain_Test
+{
+    public class Person_Test
+    {
+
+        [Fact]
+        public void コンストラクタ_初期値() { }
+    }
+}
