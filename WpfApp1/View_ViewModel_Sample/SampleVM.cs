c7773be6602a275a5395ae6689bb2800538e8bd5
kei ito <ito.kei@jvckenwood.com>
2023-09-07 16:41:03 +0900

    サンプルModel追加

--- WpfApp1/View_ViewModel_Sample/SampleVM.cs
+++ WpfApp1/View_ViewModel_Sample/SampleVM.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace WpfApp1
 {
@@ -13,9 +15,11 @@ namespace WpfApp1
     /// しかし実体は、Viewのコードビハインド（partial）。
     /// View操作を柔軟にするために、ViewとViewModelを一体化した。（MVVMの軽量カスタム）
     /// </summary>
-    public partial class Sample
+    public partial class Sample : INotifyPropertyChanged
     {
         #region Fields
+
+        private Domain.Sample.Sample _model;
 
         #endregion
 
@@ -28,7 +32,14 @@ namespace WpfApp1
         /// <summary>
         /// サンプルテキスト
         /// </summary>
-        public string Text { get => "AAAAAAAAAa"; }
+        public string Text { get => _model.Text; }
+
+        /// <summary>
+        /// 不要だが、バインドした際のメモリリーク対策のため追加 INotifyPropertyChanged
+        /// </summary>
+#pragma warning disable CS0067 // イベント 'Sample.PropertyChanged' は使用されていません
+        public event PropertyChangedEventHandler? PropertyChanged;
+#pragma warning restore CS0067 // イベント 'Sample.PropertyChanged' は使用されていません
 
         #endregion
 
@@ -39,6 +50,7 @@ namespace WpfApp1
         /// </summary>
         private void InitializeViewModel()
         {
+            _model = new();
         }
 
         #endregion
