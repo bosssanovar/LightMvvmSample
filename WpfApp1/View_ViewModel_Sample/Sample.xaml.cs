527132652de816a47dabcf0b054786f22fe26d9e
kei ito <ito.kei@jvckenwood.com>
2023-09-08 09:54:08 +0900

    Viewのコンストラクタ　アウトライン用スニペット追加

--- WpfApp1/View_ViewModel_Sample/Sample.xaml.cs
+++ WpfApp1/View_ViewModel_Sample/Sample.xaml.cs
@@ -14,7 +14,12 @@ namespace WpfApp1
         /// </summary>
         public Sample()
         {
-            // Init VM
+            #region init View Members
+
+            #endregion
+
+            #region init ViewModel Members
+
             _model = new();
 
             Text = _model.Text.ToReactivePropertySlimAsSynchronized(
@@ -22,6 +27,8 @@ namespace WpfApp1
                 x => x.Text,             // Convert
                 x => new SampleTextVO(x) // ConvertBack
                 );
+
+            #endregion
 
             InitializeComponent();
         }
