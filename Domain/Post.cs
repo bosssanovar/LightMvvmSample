bf0b3818388bc3c4960c005ba7e7d07baf8f5960
kei ito <ito.kei@jvckenwood.com>
2023-10-13 08:51:22 +0900

    役職列挙子を追加

---
+++ Domain/Post.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    /// <summary>
+    /// 役職　列挙子
+    /// </summary>
+    public enum Post : int
+    {
+        /// <summary>
+        /// 従業員
+        /// </summary>
+        Employee = 0,
+
+        /// <summary>
+        /// 主任
+        /// </summary>
+        Chief = 1,
+
+        /// <summary>
+        /// 課長
+        /// </summary>
+        SectionChief = 2,
+
+        /// <summary>
+        /// 部長
+        /// </summary>
+        Manager = 3,
+
+        /// <summary>
+        /// 役員
+        /// </summary>
+        Director = 4,
+
+        /// <summary>
+        /// 社長
+        /// </summary>
+        President = 5,
+    }
+
+    /// <summary>
+    /// <see cref="Post"/>列挙子の拡張メソッド
+    /// </summary>
+    public static partial class PostExtend
+    {
+        /// <summary>
+        /// 表示用文字列を取得します。
+        /// </summary>
+        /// <param name="post"><see cref="Post"/>列挙子</param>
+        /// <returns>表示用文字列</returns>
+        public static string GetDisplayText(this Post post)
+        {
+            return post switch
+            {
+                Post.Employee => "従業員",
+                Post.Chief => "主任",
+                Post.SectionChief => "課長",
+                Post.Manager => "部長",
+                Post.Director => "役員",
+                Post.President => "社長",
+                _ => throw new ArgumentOutOfRangeException(nameof(post), "未定義です"),
+            };
+        }
+    }
+}
---
+++ Domain_Test/Post_Test.cs
@@ -0,0 +1,48 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Entity_Test
+{
+    public class Post_Test
+    {
+        [Fact]
+        public void 表示用文字列の定義漏れ監視()
+        {
+            var isException = false;
+            var a = (Post)6;
+            try
+            {
+                var _ = a.GetDisplayText();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                isException = true;
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+            Assert.True(isException);
+        }
+
+        [Theory]
+        [InlineData(Post.Employee, Post.Chief)]
+        [InlineData(Post.Chief, Post.SectionChief)]
+        [InlineData(Post.SectionChief, Post.Manager)]
+        [InlineData(Post.Manager, Post.Director)]
+        [InlineData(Post.Director, Post.President)]
+        public void 比較(Post low, Post high)
+        {
+            Assert.True(low < high);
+            Assert.False(low == high);
+        }
+    }
+
+}
