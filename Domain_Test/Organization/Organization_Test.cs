c87167e029af8a336b7dde6d2f5c5f5a42d0cdc4
kei ito <ito.kei@jvckenwood.com>
2023-10-18 10:18:42 +0900

    テストコード整理

---
+++ Domain_Test/Organization/Organization_Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Entity_Test.Organization
+{
+    public class Organization_Test
+    {
+
+        [Fact]
+        void 社員の所属組織取得()
+        {
+            // TODO K.I : テスト未実装
+        }
+
+        [Fact]
+        public void 社員の部署移動()
+        {
+            // TODO K.I : テスト未実装
+        }
+
+        [Fact]
+        public void 組織長交代()
+        {
+            // TODO K.I : テスト未実装
+        }
+    }
+}
